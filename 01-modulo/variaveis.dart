0dd07da3804bdaa1be07501b49306ceea3bb0f80
Ivana Moreira <118234286+Ivyxp@users.noreply.github.com>
2023-10-25 11:14:59 -0300

    Add files via upload
    variaveis e tipagem de dados em dart

---
+++ 01-modulo/variaveis.dart
@@ -0,0 +1,19 @@
+// Aprendendo sobre tipos de dados e variaveis em dart
+
+String nome = "aprendendo";
+int ano = 2023;
+double decimos = 3.14;
+List<String> herois = ["Superman", "Wonder Woman", "batman"];
+
+//declaração explícita do tipo da variável (String)
+String name = 'Fusca I';
+
+//declaração de variável sem especificar seu tipo
+var namee = 'Fusca I';
+
+// Dart identifica o tipo da variável
+
+var nome2 = 'aprendendo sobre tipagem';
+var ano2 = 2023;
+var decimos2 = 3.7;
+var herois2 = ["Superman", "Wonder Woman", "batman"];
