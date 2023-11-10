5f9cfd5f22fd6c0eca03c48059362089a250968c
Ivana Moreira <118234286+Ivyxp@users.noreply.github.com>
2023-11-10 11:03:22 -0300

    Update funcoes.dart

--- 01-modulo/funcoes.dart
+++ 01-modulo/funcoes.dart
@@ -1,30 +1,21 @@
-//criando uma classe carro
-class Carro {
-//Atributos
-  String nome;
-  String modelo;
-  int ano;
-  
-//Construtor
-  Carro(this.nome, this.modelo, this.ano);
-
-//Metodos
-  void acelerar() {
-    print('o $nome $modelo esta acelerando!');
-  }
-
-  void frear() {
-    print('o $nome $modelo esta freando!');
-  }
-}
-
-//main
 void main() {
 
-//instancia da classe
-  Carro car = Carro('toyota', 'corolla', 2002);
+boasVIndas();
 
-// chamando os metodos
-  car.acelerar();
-  car.frear();
+print(soma(25, 2));
+  
+print(calcularArea(10));
+  
 }
+
+void boasVIndas(){
+  String nome = 'Lina';
+  print('bem vinda, ${nome}');
+}
+
+double soma(double valor1, double valor2){
+  double resultado = valor1 + valor2;
+  return resultado;
+}
+
+double calcularArea(double raio) => 3.14 * raio * raio;
